53fe4f94f34ebd859c26b960d1d90ce54c2bc428
khali <khali@192.168.0.10>
2020-04-20 18:04:29 +0200

    Skapar Kalender

--- src/oru_scrum_projekt/Kalender.java
+++ src/oru_scrum_projekt/Kalender.java
@@ -7,8 +7,11 @@ package oru_scrum_projekt;
 
 
 
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
+
 
 
 /**
@@ -35,6 +38,7 @@ public class Kalender extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        calendar = new com.toedter.calendar.JCalendar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,6 +52,8 @@ public class Kalender extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Tahoma", 2, 24)); // NOI18N
         jLabel2.setText("Kalender");
 
+        calendar.setBackground(new java.awt.Color(255, 255, 255));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -58,6 +64,11 @@ public class Kalender extends javax.swing.JFrame {
                     .addComponent(jButton1)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(417, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(76, 76, 76)
+                    .addComponent(calendar, javax.swing.GroupLayout.PREFERRED_SIZE, 387, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(76, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -67,11 +78,27 @@ public class Kalender extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 225, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(48, 48, 48))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(67, 67, 67)
+                    .addComponent(calendar, javax.swing.GroupLayout.DEFAULT_SIZE, 223, Short.MAX_VALUE)
+                    .addGap(67, 67, 67)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
       
+     private void btnMeetingsActionPerformed(java.awt.event.ActionEvent evt) {                                            
+
+        DateFormat formatDatum = new SimpleDateFormat("yyyy-MM-dd");
+        String datum = formatDatum.format(calendar.getDate());
+        System.out.println(datum);
+        datumMote = datum;
+
+        new Moten().setVisible(true);
+
+
+    }   
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         this.dispose();
         new InloggadAnvandare().setVisible(true);
@@ -105,14 +132,13 @@ public class Kalender extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Kalender().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Kalender().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.toedter.calendar.JCalendar calendar;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
