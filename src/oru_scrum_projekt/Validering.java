96042be87ef008eec804ad5fbd02d2a8025b975c
Hugosterberg <59054135+Hugosterberg@users.noreply.github.com>
2020-04-21 14:42:14 +0200

    ...

---
+++ src/oru_scrum_projekt/Validering.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oru_scrum_projekt;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author HugoO
+ */
+public class Validering {
+    
+    Connection con = null;
+    
+    public Validering()
+    {
+        try
+        {
+        Databaskoppling fdb = new Databaskoppling();
+        con = fdb.connect();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error");
+        }
+    }
+    
+    public boolean RattLosenord(JTextField namn, JPasswordField losenord){
+       // String namn = 
+        try{
+             ResultSet anvandarnamn = con.createStatement().executeQuery("SELECT LÖSENORD FROM ANVÄNDARNAMN WHERE NAMN = ");
+        }
+        catch (Exception e){
+             System.out.println("Error");
+        }
+        
+        return true;
+        
+    }
+    
+    
+}
