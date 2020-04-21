0d78d08b7f56ba3cf726231360b12bb2f79c064d
Hugosterberg <59054135+Hugosterberg@users.noreply.github.com>
2020-04-21 14:13:18 +0200

    Fixat mainmetoden och lagt till SQL-frågor

--- src/oru_scrum_projekt/ORU_Scrum_Projekt.java
+++ src/oru_scrum_projekt/ORU_Scrum_Projekt.java
@@ -18,12 +18,50 @@ public class ORU_Scrum_Projekt {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
-        //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        //String connectionURL = "jdbc:sqlserver://(localdb)\\MSSQLLocalDB;databaseName=ORU_Forum;user=root;password=root;";
-        //Connection con = DriverManager.getConnection(connectionURL);
+        Connection myConn = null;
+        Statement myStatement = null;
+        ResultSet myResultset = null;
         
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionURL = "jdbc:sqlserver://localhost:1433;databaseName=ForumDB;user=admin;password=admin";
+        
+        try{
+            //Skapa koppling till databas
+            myConn = DriverManager.getConnection(connectionURL);
+        
+            //Skapa statement
+            myStatement = myConn.createStatement();
+        
+            //Kör SQL fråga
+            myResultset = myStatement.executeQuery("Select * from Användare");
+        
+            //Hantera ResultSet
+            while (myResultset.next())
+            {
+                System.out.println(myResultset.getString("Namn") + ", " + myResultset.getString("Lösenord"));
+            }
+        }
+        catch (Exception exc)
+        {
+            exc.printStackTrace();
+        }
+        finally 
+        {
+            if (myResultset != null)
+            {
+                myResultset.close();
+            }
+            if (myStatement != null)
+            {
+                myStatement.close();
+            }
+            if (myConn != null)
+            {
+                myConn.close();
+            }
+        }
         connect();
         new Startsida().setVisible(true);
     }
