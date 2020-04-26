5d223634453987abaa98ea29fc166c3d61662c5c
Gabbe <gabriel.barsham@hotmail.com>
2020-04-26 20:58:34 +0200

    FIX IGEN

--- src/oru_scrum_projekt/SkickaMejl.java
+++ src/oru_scrum_projekt/SkickaMejl.java
@@ -1,17 +1,18 @@
 
+/**
 package oru_scrum_projekt;
 
 import javax.swing.JOptionPane;
 
-/**
+
  *
  * @author khali
- */
+ 
 public class SkickaMejl extends javax.swing.JFrame {
 
     /**
      * Creates new form SkickaMejl
-     */
+     
     public SkickaMejl() {
         initComponents();
     }
@@ -156,13 +157,13 @@ public class SkickaMejl extends javax.swing.JFrame {
 
     /**
      * @param args the command line arguments
-     */
+     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+         
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -181,7 +182,7 @@ public class SkickaMejl extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+         
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SkickaMejl().setVisible(true);
@@ -203,3 +204,4 @@ public class SkickaMejl extends javax.swing.JFrame {
     private javax.swing.JTextField txtMejladress;
     // End of variables declaration//GEN-END:variables
 }
+*/
